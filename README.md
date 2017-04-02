69c444fc28dbf25641329bdeae7ab0677649315e
Alex Ballera <alexballera@gmail.com>
2017-04-02 19:52:43 -0400

    Terminado ruta basica

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 ## React App
 
 [https://reacttraining.com/react-router/web/guides/quick-start](https://reacttraining.com/react-router/web/guides/quick-start)
+
+    npm run watch
+
+    npm run serve
--- src/index.jsx
+++ src/index.jsx
@@ -2,6 +2,19 @@ import React, {Component} from 'react'
 import {render} from 'react-dom'
 import {BrowserRouter as Router, Route, NavLink} from 'react-router-dom'
 
+class Navigation extends Component {
+  render () {
+    return (
+      <ul>
+        <li><NavLink to='/'>Home</NavLink></li>
+        <li><NavLink to='/car'>Cars</NavLink></li>
+        <li><NavLink to='/about'>About</NavLink></li>
+        <li><NavLink to='/topics'>Topics</NavLink></li>
+      </ul>
+    )
+  }
+}
+
 class Home extends Component {
   render () {
     if (this.props.children) {
@@ -32,25 +45,49 @@ class About extends Component {
   }
 }
 
-class Navigation extends Component {
-  render () {
-    return (
-      <ul>
-        <li><NavLink to='/'>Home</NavLink></li>
-        <li><NavLink to='/car'>Cars</NavLink></li>
-        <li><NavLink to='/about'>About</NavLink></li>
-      </ul>
-    )
-  }
-}
+const Topics = ({ match }) => (
+  <div>
+    <h2>Topics</h2>
+    <ul>
+      <li>
+        <NavLink to={`${match.url}/rendering`}>
+          Rendering with React
+        </NavLink>
+      </li>
+      <li>
+        <NavLink to={`${match.url}/components`}>
+          Components
+        </NavLink>
+      </li>
+      <li>
+        <NavLink to={`${match.url}/props-v-state`}>
+          Props v. State
+        </NavLink>
+      </li>
+    </ul>
+
+    <Route path={`${match.url}/:topicId`} component={Topic} />
+    <Route exact path={match.url} render={() => (
+      <h3>Please select a topic.</h3>
+    )} />
+  </div>
+)
+
+const Topic = ({ match }) => (
+  <div>
+    <h3>{match.params.topicId}</h3>
+  </div>
+)
 
 render(
   <Router>
     <div>
       <Navigation />
+      <hr />
       <Route exact path='/' component={Home} />
       <Route path='/car' component={Car} />
       <Route path='/about' component={About} />
+      <Route path='/topics' component={Topics} />
     </div>
   </Router>,
   document.getElementById('container')
