c56bd2fc54776c474dd1b7bbff08d50509fc500b
Alex Ballera <alexballera@gmail.com>
2017-04-04 18:56:16 -0400

    url-loader funciona pero no copia imagen

--- src/home/home.component.jsx
+++ src/home/home.component.jsx
@@ -1,6 +1,7 @@
 'use strict'
 import React from 'react'
 import './home.scss'
+import imagen from '../images/disney-logo.jpg'
 
 class Home extends React.Component {
   render () {
@@ -11,6 +12,7 @@ class Home extends React.Component {
         <div className='titulo'>
           <h1>Página Home</h1>
           <h2 className='sub-titulo' >Subtitulo agregando estilo</h2>
+          <img src={imagen} />
         </div>
       )
     }
--- webpack.config.js
+++ webpack.config.js
@@ -10,111 +10,153 @@ const HtmlWebpackPlugin = require('html-webpack-plugin')
 const APP_DIR = path.resolve(__dirname, 'src')
 const BUILD_DIR = path.resolve(__dirname, 'public')
 
-const commonConfig = {
-  entry: APP_DIR + '/index.jsx',
-  output: {
-    path: BUILD_DIR,
-    filename: 'bundle.js'
-  },
-  devtool: 'eval-source-map',
-  module: {
-    rules: [
-      {
-        enforce: 'pre',
-        test: /\.jsx?$/,
-        loader: 'standard-loader',
-        exclude: /node_modules/,
-        options: {
+const developmentConfig = () => {
+  const config = {
+    entry: APP_DIR + '/index.jsx',
+    output: {
+      path: BUILD_DIR,
+      filename: 'bundle.js'
+    },
+    devtool: 'eval-source-map',
+    module: {
+      rules: [
+        {
+          enforce: 'pre',
+          test: /\.jsx?$/,
+          loader: 'standard-loader',
+          exclude: /node_modules/,
+          options: {
           // Emit errors instead of warnings (default = false)
-          error: true,
+            error: true,
           // enable snazzy output (default = true)
-          snazzy: true,
+            snazzy: true,
           // other config options to be passed through to standard e.g.
-          parser: 'babel-eslint'
-        }
-      },
-      {
-        test: /\.jsx?/,
-        include: APP_DIR,
-        exclude: /node_modules/,
-        loader: [
-          'react-hot-loader',
-          'babel-loader',
-          'eslint-loader'
-        ]
-      },
-      {
-        test: /\.scss$/,
-        loader: ExtractTextPlugin.extract({
-          fallback: 'style-loader',
-          filename: 'style.css',
-          use: [
-            {
-              loader: 'css-loader',
-              options: {
-                sourceMap: true,
-                importLoaders: 1
-              }
-            },
-            {
-              loader: 'postcss-loader',
-              options: {
-                sourceMap: 'inline'
-              }
-            },
-            {
-              loader: 'sass-loader'
-            }
-          ],
-          allChunks: true
-        })
-      }
-    ]
-  },
-  devServer: {
-    host: '0.0.0.0',
-    port: 8080,
-    contentBase: path.join(__dirname, 'public')
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      title: 'Practica React Webpack',
-      filename: './index.html',
-      inject: true | 'head' | 'body',
-      showErrors: true,
-      minify: {
-        collapseInlineTagWhitespace: true,
-        collapseWhitespace: true,
-        removeComments: true,
-        removeScriptTypeAttributes: true
-      }
-    }),
-    new ExtractTextPlugin('style.css'),
-    new webpack.optimize.UglifyJsPlugin({
-      compress: {
-        warnings: false,
-        drop_console: false
-      },
-      sourceMap: true,
-      extractComments: true
-    }),
-    new OptimizeCSSAssetsPlugin({
-      cssProcessor: cssnano,
-      cssProcessorOptions: {
-        discardComments: {
-          removeAll: true
+            parser: 'babel-eslint'
+          }
         },
-        safe: true
-      },
-      canPrint: false
-    })
-  ]
+        {
+          test: /\.jsx?/,
+          include: APP_DIR,
+          exclude: /node_modules/,
+          loader: [
+            'react-hot-loader',
+            'babel-loader',
+            'eslint-loader'
+          ]
+        },
+        {
+          test: /\.scss$/,
+          loader: ExtractTextPlugin.extract({
+            fallback: 'style-loader',
+            filename: 'style.css',
+            use: [
+              {
+                loader: 'css-loader',
+                options: {
+                  sourceMap: true,
+                  importLoaders: 1
+                }
+              },
+              {
+                loader: 'postcss-loader',
+                options: {
+                  sourceMap: 'inline'
+                }
+              },
+              {
+                loader: 'sass-loader'
+              }
+            ],
+            allChunks: true
+          })
+        },
+        {
+          test: /\.(gif|png|jpe?g|svg)$/,
+          loader: 'url-loader',
+          query: {
+            limit: 10000,
+            name: 'images/[name]-[sha512:hash:base64:7].[ext]'
+          }
+        }
+       /* {
+          test: /\.(gif|png|jpe?g|svg)$/,
+          loader: 'file-loader'
+        } */
+   /*     {
+          test: /\.(gif|png|jpe?g|svg)$/,
+          loaders: [
+            'file-loader',
+            {
+              loader: 'image-webpack-loader',
+              query: {
+                progressive: true,
+                gifsicle: {
+                  interlaced: false
+                },
+                OptiPNG: {
+                  optimizationLevel: 7
+                },
+                pngquant: {
+                  quality: '65-90',
+                  speed: 4
+                }
+              }
+            }
+          ]
+        } */
+      ]
+    },
+    devServer: {
+      host: '0.0.0.0',
+      port: 8080,
+      contentBase: path.join(__dirname, 'public'),
+      historyApiFallback: true,
+      stats: 'errors-only',
+      compress: true,
+      inline: true
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        title: 'Practica React Webpack',
+        filename: './index.html',
+        inject: true | 'head' | 'body',
+        showErrors: true,
+        minify: {
+          collapseInlineTagWhitespace: true,
+          collapseWhitespace: true,
+          removeComments: true,
+          removeScriptTypeAttributes: true
+        }
+      }),
+      new ExtractTextPlugin('style.css'),
+      new webpack.optimize.UglifyJsPlugin({
+        compress: {
+          warnings: false,
+          drop_console: false
+        },
+        sourceMap: true,
+        extractComments: true
+      }),
+      new OptimizeCSSAssetsPlugin({
+        cssProcessor: cssnano,
+        cssProcessorOptions: {
+          discardComments: {
+            removeAll: true
+          },
+          safe: true
+        },
+        canPrint: false
+      })
+    ]
+  }
+  return Object.assign(
+    {},
+    config
+  )
 }
 
 module.exports = (env) => {
-  console.log('env', env)
-
-  return commonConfig
+  return developmentConfig()
 }
 
 /*,
