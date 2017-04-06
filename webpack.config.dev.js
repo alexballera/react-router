46bcff5752b7d262779ad4187c161b57e3afb660
Alex Ballera <alexballera@gmail.com>
2017-04-06 19:55:30 -0400

    Terminado de modulado de config

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,93 +1,11 @@
+require('webpack')
+const path = require('path')
+const merge = require('webpack-merge')
+const common = require('./webpack.config.base')
+
 module.exports = (env) => {
-  const webpack = require('webpack')
-  const path = require('path')
-  const HtmlWebpackPlugin = require('html-webpack-plugin')
-  const CleanWebpackPlugin = require('clean-webpack-plugin')
-  const ExtractTextPlugin = require('extract-text-webpack-plugin')
-  const APP_DIR = path.resolve(__dirname, 'src')
-  const BUILD_DIR = path.resolve(__dirname, 'public')
-  return {
-    entry: APP_DIR + '/index.jsx',
-
-    output: {
-      path: BUILD_DIR,
-      filename: 'bundle.js',
-      sourceMapFilename: '[name].map',
-      chunkFilename: '[id].chunk.js'
-    },
-
+  return merge(common(), {
     devtool: 'eval-source-map',
-
-    module: {
-      rules: [
-        {
-          enforce: 'pre',
-          test: /\.jsx?$/,
-          loader: 'standard-loader',
-          exclude: /node_modules/,
-          options: {
-            error: true,
-            snazzy: true,
-            parser: 'babel-eslint'
-          }
-        },
-
-        // Javascript
-        {
-          test: /\.jsx?/,
-          include: APP_DIR,
-          exclude: /node_modules/,
-          loader: [
-            'react-hot-loader',
-            'babel-loader',
-            'eslint-loader'
-          ]
-        },
-
-        // Estilos
-        {
-          test: /\.scss$/,
-          loader: ExtractTextPlugin.extract({
-            fallback: 'style-loader',
-            filename: 'style.css',
-            use: [
-              {
-                loader: 'css-loader',
-                options: {
-                  sourceMap: true,
-                  importLoaders: 1
-                }
-              },
-              {
-                loader: 'postcss-loader',
-                options: {
-                  sourceMap: 'inline'
-                }
-              },
-              {
-                loader: 'sass-loader'
-              }
-            ],
-            allChunks: true
-          })
-        },
-
-        // Imagenes
-        {
-          test: /\.(gif|png|jpe?g|svg)$/,
-          loader: 'url-loader',
-          query: {
-            limit: 10000,
-            name: 'images/[name].[ext]'
-          }
-        },
-
-        // Fonts
-        { test: /\.(eot|svg|ttf|woff|woff2)$/,
-          loader: 'file-loader?name=fonts/[name]/[name].[ext]'
-        }
-      ]
-    },
 
     devServer: {
       host: '0.0.0.0',
@@ -97,27 +15,6 @@ module.exports = (env) => {
       stats: 'errors-only',
       compress: true,
       inline: true
-    },
-
-    plugins: [
-      new CleanWebpackPlugin('public'),
-      new HtmlWebpackPlugin({
-        title: 'Practica React Webpack',
-        filename: './index.html',
-        template: './src/index.ejs',
-        inject: true | 'head' | 'body',
-        minify: {
-          collapseInlineTagWhitespace: true,
-          collapseWhitespace: true,
-          removeComments: true,
-          removeScriptTypeAttributes: true
-        }
-      }),
-      new ExtractTextPlugin('style.css'),
-      new webpack.ProvidePlugin({
-        $: 'jquery',
-        jQuery: 'jquery'
-      })
-    ]
-  }
+    }
+  })
 }
--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,112 +1,13 @@
+const webpack = require('webpack')
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin')
+const cssnano = require('cssnano')
+const FaviconsWebpackPlugin = require('favicons-webpack-plugin')
+const merge = require('webpack-merge')
+const common = require('./webpack.config.base')
+
 module.exports = (env) => {
-  const webpack = require('webpack')
-  const path = require('path')
-  const ExtractTextPlugin = require('extract-text-webpack-plugin')
-  const HtmlWebpackPlugin = require('html-webpack-plugin')
-  const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin')
-  const cssnano = require('cssnano')
-  const FaviconsWebpackPlugin = require('favicons-webpack-plugin')
-
-  const APP_DIR = path.resolve(__dirname, 'src')
-  const BUILD_DIR = path.resolve(__dirname, 'public')
-  return {
-    entry: APP_DIR + '/index.jsx',
-
-    output: {
-      path: BUILD_DIR,
-      filename: 'bundle.js',
-      sourceMapFilename: '[name].map',
-      chunkFilename: '[id].chunk.js'
-    },
-
-    module: {
-      rules: [
-        {
-          enforce: 'pre',
-          test: /\.jsx?$/,
-          loader: 'standard-loader',
-          exclude: /node_modules/,
-          options: {
-            error: true,
-            snazzy: true,
-            parser: 'babel-eslint'
-          }
-        },
-
-        // Javascript
-        {
-          test: /\.jsx?/,
-          include: APP_DIR,
-          exclude: /node_modules/,
-          loader: [
-            'react-hot-loader',
-            'babel-loader',
-            'eslint-loader'
-          ]
-        },
-
-        // Estilos
-        {
-          test: /\.scss$/,
-          loader: ExtractTextPlugin.extract({
-            fallback: 'style-loader',
-            filename: 'style.css',
-            use: [
-              {
-                loader: 'css-loader',
-                options: {
-                  sourceMap: true,
-                  importLoaders: 1
-                }
-              },
-              {
-                loader: 'postcss-loader',
-                options: {
-                  sourceMap: 'inline'
-                }
-              },
-              {
-                loader: 'sass-loader'
-              }
-            ],
-            allChunks: true
-          })
-        },
-
-        // Imagenes
-        {
-          test: /\.(gif|png|jpe?g|svg)$/,
-          loader: 'url-loader',
-          query: {
-            limit: 10000,
-            name: 'images/[name].[ext]'
-          }
-        },
-
-        // Fonts
-        { test: /\.(eot|svg|ttf|woff|woff2)$/,
-          loader: 'file-loader?name=fonts/[name]/[name].[ext]'
-        }
-      ]
-    },
+  return merge(common(), {
     plugins: [
-      new HtmlWebpackPlugin({
-        title: 'Practica React Webpack',
-        filename: './index.html',
-        template: './src/index.ejs',
-        inject: true | 'head' | 'body',
-        minify: {
-          collapseInlineTagWhitespace: true,
-          collapseWhitespace: true,
-          removeComments: true,
-          removeScriptTypeAttributes: true
-        }
-      }),
-      new ExtractTextPlugin('style.css'),
-      new webpack.ProvidePlugin({
-        $: 'jquery',
-        jQuery: 'jquery'
-      }),
       new webpack.DefinePlugin({
         'process.env.NODE_ENV': JSON.stringify('production')
       }),
@@ -131,5 +32,5 @@ module.exports = (env) => {
       }),
       new FaviconsWebpackPlugin({logo: './favicon.png', inject: true})
     ]
-  }
+  })
 }
