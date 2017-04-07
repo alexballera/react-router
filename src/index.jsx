67e448753b5440b28e78d4b58d9380d067b956d4
Alex Ballera <alexballera@gmail.com>
2017-04-06 20:27:49 -0400

    Prueba de jquery

--- src/index.jsx
+++ src/index.jsx
@@ -7,11 +7,14 @@ import './index.scss'
 import 'font-awesome/scss/font-awesome.scss'
 import '../node_modules/materialize-css/sass/materialize.scss'
 import 'materialize-css'
+import prueba from './static/js/jquery.prueba'
 
 (() => {
   var div = document.createElement('div')
   div.setAttribute('id', 'app')
   document.body.prepend(div)
+
+  prueba()
 })()
 
 render(
---
+++ src/static/js/jquery.prueba.js
@@ -0,0 +1,8 @@
+'use strict'
+import $ from 'jquery'
+
+module.exports = () => {
+  $(function () {
+    $('#app').addClass('probando')
+  })
+}
