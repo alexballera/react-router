09939c161cdda42bf6ec583956f471e3a47dc663
Alex Ballera <alexballera@gmail.com>
2017-04-08 15:26:25 -0400

    Optimizado index

--- src/index.jsx
+++ src/index.jsx
@@ -1,16 +1,19 @@
 import React from 'react'
 import {render} from 'react-dom'
-import Routers from './router.jsx'
-import 'modernizr'
 
+// Jquery
 window.jQuery = require('jquery')
 window.$ = require('jquery')
 
+// Fuentes, estilos y optimizacion
 import './index.scss'
 import 'font-awesome/scss/font-awesome.scss'
 import '../node_modules/materialize-css/sass/materialize.scss'
 import 'materialize-css'
+import 'modernizr'
 
+// Components
+import Routers from './router.jsx'
 import prueba from './static/js/jquery.prueba'
 import customDom from './static/js/custom-dom'
 
